ba2a89385d1e5904b3152da8e6fccd081dc8c0bf
ChloPUPs <alexplaysgamezzz@outlook.com>
2025-03-27 19:06:34 -0700

    improve number multiplier

--- numbermultiplier/index.js
+++ numbermultiplier/index.js
@@ -3,11 +3,17 @@ const getNumber1 = document.getElementById("number1");
 const getNumber2 = document.getElementById("number2");
 const getAnswer = document.getElementById("answer");
 
-submitNumbers.onclick = function(){
+getSubmitNumbers.onclick = function(){
     let number1 = getNumber1.value;
     let number2 = getNumber2.value;
     number1 = Number(number1);
     number2 = Number(number2);
-    let answer = number1 * number2;
-    getAnswer.textContent = answer;
+    if(isNaN(number1) || isNaN(number2)){
+        console.log("a");
+        getAnswer.textContent = "That isn't a number";
+    }
+    else{
+        let answer = number1 * number2;
+        getAnswer.textContent = answer;
+    }
 }
